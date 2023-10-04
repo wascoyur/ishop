ec0d06fc58c7c5853c26a28376104b6e07964862
yuriy.vasilev <mytem20007@gmail.com>
2023-10-04 23:46:13 +0500

    eslint, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
-import { useState } from 'react'
-
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {/*<AppHeader />*/}
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="profile" element={<RedoUserProfileForm />} />
+        <Route path="products" element={<ProductPage />} />
+        <Route path="products/edit-products" element={<AddProductForm />} />
+        <Route
+          path="products/product-card/:productId"
+          element={<ProductCard />}
+        />
+
+        <Route path="create-product" element={<AddProductForm />} />
+        <Route path="bucket" element={<PageBucket />} />
+        <Route path="auth" element={<LoginPage />} />
+        <Route path="register" element={<RegisterUser />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
