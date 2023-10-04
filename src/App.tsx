0a764bc704946c1ad3aea68d03502cd62a3a9d06
yuriy.vasilev <mytem20007@gmail.com>
2023-10-05 00:01:09 +0500

    some packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/*<AppHeader />*/}
       <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="profile" element={<RedoUserProfileForm />} />
-        <Route path="products" element={<ProductPage />} />
-        <Route path="products/edit-products" element={<AddProductForm />} />
-        <Route
-          path="products/product-card/:productId"
-          element={<ProductCard />}
-        />
+        {/*<Route index element={<HomePage />} />*/}
+        {/*<Route path="profile" element={<RedoUserProfileForm />} />*/}
+        {/*<Route path="products" element={<ProductPage />} />*/}
+        {/*<Route path="products/edit-products" element={<AddProductForm />} />*/}
+        {/*<Route*/}
+        {/*  path="products/product-card/:productId"*/}
+        {/*  element={<ProductCard />}*/}
+        {/*/>*/}
 
-        <Route path="create-product" element={<AddProductForm />} />
-        <Route path="bucket" element={<PageBucket />} />
-        <Route path="auth" element={<LoginPage />} />
-        <Route path="register" element={<RegisterUser />} />
-        <Route path="*" element={<ErrorPage />} />
+        {/*<Route path="create-product" element={<AddProductForm />} />*/}
+        {/*<Route path="bucket" element={<PageBucket />} />*/}
+        {/*<Route path="auth" element={<LoginPage />} />*/}
+        {/*<Route path="register" element={<RegisterUser />} />*/}
+        {/*<Route path="*" element={<ErrorPage />} />*/}
       </Routes>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import checker from 'vite-plugin-checker'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 /// <reference types="vite/client" />
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),checker({
-    // e.g. use TypeScript check
-    typescript: true,
-  }),],
-})
+  plugins: [
+    react(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+  ],
+});
