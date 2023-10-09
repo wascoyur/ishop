cb407020830cab41031386d2e7f692201930c1f3
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-10-09 15:10:02 +0500

    add Apollo

---
+++ src/shared/auth/useAuth.ts
@@ -0,0 +1,14 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+type logIn = [login: string, password: string];
+export const useAuth = (props: logIn) => {
+  let token: unknown;
+  let error: unknown;
+  const [login, password] = props;
+  const API = `http://cea3c11a3f62.vps.myjino.ru/graphql`;
+  const client = new ApolloClient({
+    uri: API,
+    cache: new InMemoryCache(),
+  });
+  return { token, error };
+};
--- src/widgets/LoginForm.tsx
+++ src/widgets/LoginForm.tsx
@@ -1,51 +1,41 @@
-import { FormEvent, useEffect, useState } from "react";
 import "../shared/common-form.scss";
 import { useNavigate } from "react-router-dom";
-import { useProfileStore } from "../app/state.ts";
+import { useRef } from "react";
 
 export const LoginForm = () => {
-  const setToken = useProfileStore((state) => state.setToken);
-  const [login, setLogin] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [isFilled, setIsFilled] = useState<boolean>(false);
+  // const setToken = useProfileStore((state) => state.setToken);
+  // const [isFilled, setIsFilled] = useState<boolean>(false);
   const navigate = useNavigate();
+  const loginRef = useRef<HTMLInputElement>(null);
+  const passRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    if (login.length > 0 && password.length > 0) {
-      setIsFilled(true);
-      return;
-    }
-    setIsFilled(false);
-  }, [login, password]);
-  const handleLogInUser = (e: FormEvent) => {
-    e.preventDefault();
-    if (login === "admin" && password === "admpass") {
-      setToken(login);
-      return;
-    }
-    if (login === "user" && password === "password") {
-      setToken(login);
-      return;
-    }
+  // useEffect(() => {
+  //   if (login.length > 0 && password.length > 0) {
+  //     setIsFilled(true);
+  //     return;
+  //   }
+  //   setIsFilled(false);
+  // }, [login, password]);
+  // const handleLogInUser = (e: FormEvent) => {
+  //   e.preventDefault();
+  //   if (login === "admin" && password === "admpass") {
+  //     setToken(login);
+  //     return;
+  //   }
+  //   if (login === "user" && password === "password") {
+  //     setToken(login);
+  //     return;
+  //   }
 
-    navigate("/register");
-  };
+  navigate("/register");
 
   return (
     <div className="default-style">
       <form onSubmit={handleLogInUser}>
-        <label htmlFor="login">Ваш логин: user/admin</label>
-        <input
-          type="text"
-          name="login"
-          onChange={(e) => setLogin(e.target.value)}
-        />
-        <label htmlFor="password">Введите пароль: password/admpass</label>
-        <input
-          type="password"
-          name="passwoord"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <label htmlFor="login">Ваш логин</label>
+        <input type="text" name="login" ref={loginRef} />
+        <label htmlFor="password">Введите пароль</label>
+        <input type="password" name="passwoord" ref={passRef} />
         <button type={"submit"} disabled={!isFilled}>
           Войти
         </button>
